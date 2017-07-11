0cefa199a61478e5301e58b322d1937952ecada6
Alana Franklin <alanafrankly@gmail.com>
2017-07-11 09:29:26 -0700

    Added structure for prototype entry function

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,13 @@
 'use strict';
 
-function Entry(name,thumbnail,url){
+function Entry(name,thumbnail,url,description){
   this.name = name;
   this.author = 'Alana Franklin';
   this.thumbnail = 'img/' + thumbnail;
   this.url = url;
+  this.description = description;
+}
+
+Entry.prototype.addEntry(){
+
 }
