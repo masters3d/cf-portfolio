015d222946bd99cbf6e7e0c539befda2efd8a585
Alana Franklin <alanafrankly@gmail.com>
2017-07-11 21:06:34 -0700

    Addng portfolio items dynamically from template and data source

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,42 @@
 'use strict';
 
+var entries = [];
 
 $(document).ready(function(){
   $('#hamburger').click(function(){
     $('header ul').toggle();
   })
+
+  portfolioContent.forEach(function(entryObject) {
+    entries.push(new Entry(entryObject));
+  });
+
+  entries.forEach(function(entry) {
+    $('#portfolio').append(entry.toHtml());
+  });
+
 });
 
-
-function Entry(name, thumbnailUrl, url, description){
-  this.name = name;
+function Entry(rawData){
+  this.name = rawData.name;
   this.author = 'Alana Franklin';
-  this.thumbnailUrl = thumbnailUrl;
-  this.url = url;
-  this.description = description;
+  this.thumbnailUrl = rawData.thumbnailUrl;
+  this.url = rawData.url;
+  this.description = rawData.description;
 }
+
+Entry.prototype.toHtml = function() {
+  var $newEntry = $('article.template').clone();
+  $newEntry.removeClass('template');
+
+  if (!this.publishedOn) $newEntry.addClass('draft');
+
+  $newEntry.find('h3').text(this.name);
+  $newEntry.find('p').text(this.description);
+  $newEntry.find('a').attr('href', this.url);
+  $newEntry.find('a').attr('alt', this.name);
+  $newEntry.find('img').attr('alt', this.name);
+  $newEntry.find('img').attr('src', this.thumbnailUrl);
+
+  return $newEntry;
+};
--- scripts/portfolioContent.js
+++ scripts/portfolioContent.js
@@ -4,20 +4,20 @@ var portfolioContent = [
   {
     name: 'Periodic Table',
     author: 'Alana Franklin',
-    thumbnail: 'http://www.resume.alanafrankly.com/projects/Periodic-Table/thumbnail.png',
+    thumbnailUrl: 'http://www.resume.alanafrankly.com/projects/Periodic-Table/thumbnail.png',
     url: 'http://www.resume.alanafrankly.com/projects/Periodic-Table/index.html',
     description: 'Interactive Periodic Table'
   }, {
     name: 'Music Boxes',
     author: 'Alana Franklin',
-    thumbnail: 'http://www.resume.alanafrankly.com/projects/musicBox/thumbnail.png',
+    thumbnailUrl: 'http://www.resume.alanafrankly.com/projects/musicBox/thumbnail.png',
     url: 'http://www.resume.alanafrankly.com/projects/musicBox/index.html',
     description: 'Interactice music box experience utilizing jQuery, javascript, CSS, and HTML.'
   }, {
     name: 'Etch-a-Sketch',
     author: 'Alana Franklin',
-    thumbnail: 'http://www.resume.alanafrankly.com/projects/sketchpad/thumbnail.png',
+    thumbnailUrl: 'http://www.resume.alanafrankly.com/projects/sketchpad/thumbnail.png',
     url: 'http://www.resume.alanafrankly.com/projects/sketchpad/index.html',
     description: 'An recreation of an Etch-a-Sketch using Javascript.'
   }
-]
+];
