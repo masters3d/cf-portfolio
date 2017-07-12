a265075ab354a0c74124215bd737eb3050a95a95
Alana Franklin <alanafrankly@gmail.com>
2017-07-11 21:30:25 -0700

    Refactored html/css. Styles portfolio div and made responsive.

--- scripts/portfolioContent.js
+++ scripts/portfolioContent.js
@@ -6,13 +6,13 @@ var portfolioContent = [
     author: 'Alana Franklin',
     thumbnailUrl: 'http://www.resume.alanafrankly.com/projects/Periodic-Table/thumbnail.png',
     url: 'http://www.resume.alanafrankly.com/projects/Periodic-Table/index.html',
-    description: 'Interactive Periodic Table'
+    description: 'Interactive Periodic Table using JavaScript'
   }, {
     name: 'Music Boxes',
     author: 'Alana Franklin',
     thumbnailUrl: 'http://www.resume.alanafrankly.com/projects/musicBox/thumbnail.png',
     url: 'http://www.resume.alanafrankly.com/projects/musicBox/index.html',
-    description: 'Interactice music box experience utilizing jQuery, javascript, CSS, and HTML.'
+    description: 'Interactice music box experience utilizing JavaScript/jQuery.'
   }, {
     name: 'Etch-a-Sketch',
     author: 'Alana Franklin',
