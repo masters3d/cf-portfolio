90e3356061aa5a8a59148ac3d90bfc2e61746abe
Alana Franklin <alanafrankly@gmail.com>
2017-07-18 10:04:07 -0700

    Implemented Article.loadData to load AJAX data to screen

--- scripts/app.js
+++ scripts/app.js
@@ -1,49 +1,31 @@
 'use strict';
 
-var Article = {}; // article for methods to live on
+var PortItem = {}; // PortItem for methods to live on
 
-function renderTemplate(){
-  portfolioContent.forEach(function(dataObj){
-    var $templateString = $('#handlebarsTemplate').html(); //what we're getting from HTML
-    var compiled = Handlebars.compile($templateString); // returns function that can take data
-    var html = compiled(dataObj); //HTML with everything filled in
-    $('#portfolio').append(html); // Add it to the page
-  })
-}
-
-Article.fetchData = function(){
+PortItem.fetchData = function(){
   if (localStorage.portfolioContent){
-    Article.loadData(JSON.parse(localStorage.getItem('portfolioContent')));
+    PortItem.loadData(JSON.parse(localStorage.getItem('portfolioContent')));
   } else {
     let path = '/data/portfolioContent.json';
     $.getJSON(path).then(success, failure);
   }
 
   function success(data){
-    Article.loadData(data);
+    PortItem.loadData(data);
     localStorage.setItem('portfolioContent', JSON.stringify(data));
   }
 
   function failure(error){
-    console.err(error);
+    console.error(error);
   }
 }
 
 
-Article.loadData = function(data){
-  console.log('loading data!');
-  console.log(data);
+PortItem.loadData = function(data){
+  data.forEach(function(dataObj){
+    var $templateString = $('#handlebarsTemplate').html(); //what we're getting from HTML
+    var compiled = Handlebars.compile($templateString); // returns function that can take data
+    var html = compiled(dataObj); //HTML with everything filled in
+    $('#portfolio').append(html); // Add it to the page
+  })
 }
-
-$(document).ready(function(){
-  // portfolioContent.forEach(function(entryObject) {
-  //   entries.push(new Entry(entryObject));
-  // });
-  //
-  // entries.forEach(function(entry) {
-  //   $('#portfolio').append(entry.toHtml());
-  // });
-
-  // renderTemplate();
-
-});
