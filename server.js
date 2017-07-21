d604687b33b8693c11caedc2bc985b1d16873e51
J Cheyo Jimenez <cheyo@masters3d.com>
2017-07-21 10:38:48 -0700

    Add console.info to server when connected. Closes #16

--- server.js
+++ server.js
@@ -4,4 +4,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.static('./public'));
-app.listen(PORT, function(){});
+app.listen(PORT, function(){
+  console.info(`Listening on port ${PORT}`);
+  console.info(`http://localhost:${PORT}/`);
+});
